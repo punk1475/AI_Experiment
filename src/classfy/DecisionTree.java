85c0cf34363ff1fabaf18aa3279b3ff4d1434a1e
25807 <258073>
2023-12-26 14:43:48 +0800

    添加了注释

--- src/classfy/DecisionTree.java
+++ src/classfy/DecisionTree.java
@@ -1,6 +1,5 @@
 package classfy;
 
-import com.sun.jdi.ClassType;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,6 +23,11 @@ public class DecisionTree {
 		readFile("testdata",new File(testFilePath));
 	}
 
+	/**
+	 * 读取数据文件，并生成对应数据集合
+	 * @param fileType
+	 * @param file
+	 */
 	private void readFile(String fileType,File file){
 		try {
 			FileReader fr = new FileReader(file);
@@ -57,6 +61,13 @@ public class DecisionTree {
 	}
 
 	//计算log2(x)
+
+	/**
+	 * 用于计算log2(x)
+	 * @param value 值
+	 * @param base 基数
+	 * @return 以base为低，value的对数值
+	 */
 	static public double log(double value, double base)
 	{
 		return Math.log(value) / Math.log(base);
@@ -65,8 +76,11 @@ public class DecisionTree {
 	//计算信息熵
 	public double calcShannonEnt(ArrayList<Data> data){
 		Map<Double,Integer> typeMap=new HashMap<>();
+		//遍历数据集合
 		for(Data valueList:data){
+			//取每个数据的类别值
 			Double type=valueList.dataValue[attrCount-1];
+			//将类别放入typeMap中进行统计
 			if(typeMap.containsKey(type)){
 				int count=typeMap.get(type);
 				count++;
@@ -78,23 +92,38 @@ public class DecisionTree {
 		double sum=0;
 		Iterator<Double> it=typeMap.keySet().iterator();
 		int size=data.size();
+		//通过迭代器遍历，获取typeMap中各类别的数量，并以此计算香农熵
 		while (it.hasNext()){
 			int count=typeMap.get(it.next());
 			double p=(double) count/(double) size;
 			sum+=(-p)*log(p,2);
 		}
+		//返回计算出的熵
 		return sum;
 	}
 
 	
-	//划分数据集
+
+
+	/**
+	 * 用于划分数据集
+	 * @param data 输入的数据集合
+	 * @param featureIndex 选取的属性索引值
+	 * @param splitPoint 该数据集按照选取属性从小到大排列后，作为分裂点的数据的索引
+	 * @return 按分裂点划分出的两个数据集
+	 */
 	private ArrayList<Data>[] splitDataSet(ArrayList<Data> data, int featureIndex, int splitPoint) {
 		ArrayList<Data> sub1=new ArrayList<>();
 		ArrayList<Data> sub2=new ArrayList<>();
+		//设定数据进行比较的维度
 		chooser.setIndex(featureIndex);
+		//按此维度的值进行排序，升序
 		Collections.sort(data);
+		//用于保存未使用过的属性
 		boolean[] boolArray=Arrays.copyOf(data.get(0).unusedAttr,data.get(0).unusedAttr.length);
+		//将本次使用的属性从上面划去
 		boolArray[featureIndex]=false;
+		//遍历数据集，将索引小于splitPoint的放入sub1，将大于的放入sub2
 		for(int i=0;i<splitPoint;i++){
 			Data item=data.get(i);
 			Data newData=new Data(item.dataValue, chooser,boolArray);
@@ -109,39 +138,57 @@ public class DecisionTree {
 		return new ArrayList[]{sub1,sub2};
 	}
 	
-	//返回信息增益最大的划分属性,及对应的分裂值
+
+
+	/**
+	 * 返回信息增益最大的划分属性,及对应的分裂值
+	 * @param data 对应数据集
+	 * @return 第一个元素为属性索引，第二个为分裂索引
+	 */
 	int[] chooseBestFeatureToSplit(ArrayList<Data> data)
 	{
 		int selectedIndex=0;
 		int splitPoint=-1;
 		double minEnt=Double.MAX_VALUE;
+		//遍历全部的属性，选取Gain最大，即分裂后的平均熵最小的属性
 		for(int i=0;i<attrCount-1;i++){
 			double[] res=getEnt(data,i);
+			//为空，说明对应属性已经使用过
 			if(res==null){
 				continue;
 			}
+			//取熵最低的属性及对应分裂点
 			if(res[0]<minEnt){
 				minEnt=res[0];
 				splitPoint=(int)res[1];
 				selectedIndex=i;
 			}
 		}
+		//表明没有可用的属性
 		if(minEnt==Double.MAX_VALUE){
 			return null;
 		}
+		//返回得到的结果
 		return new int[]{selectedIndex,splitPoint};
 	}
 
-	//计算每种属性划分后的信息增益,并返回对应的分裂点
+	/**
+	 * 计算每种属性划分后的信息增益,并返回对应的分裂点
+	 * @param dataList 数据集
+	 * @param index 属性索引
+	 * @return 第一个为分裂后的平均信息熵，相当于信息增益
+	 */
 	double[] getEnt(ArrayList<Data> dataList,int index){
 		if(!dataList.get(0).unusedAttr[index]){
 			return null;
 		}
+		//设置比较的维度并排序
 		chooser.setIndex(index);
 		Collections.sort(dataList);
 		int size=dataList.size();
 		double minEnt=Double.MAX_VALUE;
 		int splitPoint=-1;
+		//从1开始尝试分裂，选择分裂后平均信息熵最小的数据点的索引
 		for(int i=1;i<size;i++){
 			ArrayList<Data> sub1=new ArrayList<>(dataList.subList(0,i)) ;
 			ArrayList<Data> sub2=new ArrayList<>(dataList.subList(i,size));
@@ -155,13 +202,19 @@ public class DecisionTree {
 		return res;
 	}
 	
-	//投票，返回支持度最高的类标签
+
+
+	/**
+	 * 投票，返回支持度最高的类标签
+	 * @param classList 一个存放对应数据集的全部类别值的集合
+	 * @return 支持度最高的类
+	 */
 	Double majorityCount(Vector<Double> classList)
 	{
 		HashMap<Double, Integer> classCount = new HashMap<>();
 		int maxVote = 0;
 		Double majorityClass = null;
-		
+		//遍历集合，统计出各类别的数量
 		for (Double clazz : classList) {
 			double classType=clazz;
 			if (!classCount.containsKey(classType)) {
@@ -172,7 +225,7 @@ public class DecisionTree {
 				classCount.put(classType, classCount.get(classType) + 1);
 			}
 		}
-		
+		//再遍历选出数量最多的类
 		Iterator<Double> iterator = classCount.keySet().iterator();
 		while (iterator.hasNext()) {
 			double key = iterator.next();
@@ -184,16 +237,20 @@ public class DecisionTree {
 		return majorityClass;	
 	}
 
-	//创建决策树
+
+	/**
+	 * 递归创建决策树
+	 * @param dataSet 数据集
+	 * @return 一个Map映射，通过递归的构造形成树
+	 */
 	HashMap<String, Object> createTree(ArrayList<Data> dataSet)
 	{
 		Vector<Double> classList = new Vector<>();
 		HashMap<String, Object> myTree = new HashMap<>();
-
+		//遍历数据集，获取类别集合
 		for (Data data : dataSet) {
 			classList.add(data.dataValue[attrCount - 1]);
 		}
-		//TO-DO:
 		//判断该分支下实例的类别是否全部相同，相同则停止划分，并返回该分支（叶子）
 		Set<Double> classSet=new HashSet<>(classList);
 		if(classSet.size()==classList.size()){
@@ -201,31 +258,37 @@ public class DecisionTree {
 			myTree.put("isLeaf",true);
 		}
 		//已经没有划分属性，类别使用多数表决，返回分支
-		int[] res=chooseBestFeatureToSplit(dataSet);
+		int[] res=chooseBestFeatureToSplit(dataSet);//选取最佳划分属性
 		if(res==null){
 			Double maxType=majorityCount(classList);
 			myTree.put("type",maxType);
 			myTree.put("isLeaf",true);
 		}
-		
 		//选取最佳的划分特征
 		else{
 			myTree.put("isLeaf",false);
 			myTree.put("chooseAttr",res[0]);
+			//按最佳划分属性排序
 			dataSet.get(0).chooser.setIndex(res[0]);
 			Collections.sort(dataSet);
 			double splitValue=dataSet.get(res[1]).dataValue[res[0]];
+			//设置spltValue属性作为后面分类时比较的依据，其值为排序后对应索引的该属性值
 			myTree.put("splitValue",splitValue);
+			//按最佳划分属性进行划分
 			ArrayList<Data>[] childDataSet=splitDataSet(dataSet,res[0],res[1]);
+			//对于划分出的俩个子集
 			for(int i=0;i<2;i++){
 				HashMap<String,Object>childTree;
+				//如果子集为空，作为叶子节点，以本次集合中的多数类作为其类别
 				if(childDataSet[i].size()==0){
 					childTree=new HashMap<>();
 					childTree.put("type",majorityCount(classList));
 					childTree.put("isLeaf",true);
 				}else {
+					//否则进行递归的构建
 					childTree=createTree(childDataSet[i]);
 				}
+				//将子集形成的节点映射到本节点中
 				if(myTree.containsKey("child")){
 					HashMap<String,Object>childTrees= (HashMap<String, Object>) myTree.get("child");
 					if(i==0){
@@ -247,14 +310,22 @@ public class DecisionTree {
 		//递归地构建决策树
 		return myTree;
 	}
-	
-	//遍历树丛根到叶子，从而得到预测结果
+
+	/**
+	 * 用于预测，遍历树丛根到叶子，从而得到预测结果
+	 * @param tree 形成的树
+	 * @param testData 测试数据
+	 * @return 预测的类别
+	 */
 	Double classify(HashMap<String, Object> tree, Data testData)
 	{
+		//如果不是叶子，继续向下走
 		while (!(boolean) tree.get("isLeaf")){
+			//得到划分属性的索引。并选择对应属性值与节点中参考值比较
 			int chooseAttr=(int) tree.get("chooseAttr");
 			double splitValue=(double)tree.get("splitValue");
 			HashMap<String,Object> children= (HashMap<String, Object>) tree.get("child");
+			//根据比较的结果进入对应节点
 			if(testData.dataValue[chooseAttr]<splitValue){
 				tree= (HashMap<String, Object>) children.get("less");
 			}else{
@@ -264,20 +335,30 @@ public class DecisionTree {
 		return (Double) tree.get("type");
 	}
 
+	/**
+	 * 用于计算测试集预测的准确率
+	 */
 	public  void predict(){
 		int correct=0;
+		//用训练集构建决策树
 		HashMap<String,Object> decisionTree=createTree(dataSet);
+		//依次对测试集全部数据进行预测，并记录结果
 		for(Data test:testDataSet){
 			if(classify(decisionTree,test)==test.dataValue[attrCount-1]){
 				correct++;
 			}
 		}
+		//打印出记录的准确率
 		System.out.println("预测正确率为:"+(double)((double)correct/(double)testDataSet.size()));
 	}
 }
 
+/**
+ * 封装出的一个数据类
+ */
 class Data implements Comparable<Data>{
 	double[] dataValue;
+	//比较器类，用于选择数据比较的维度
 	ComparatorChooser chooser;
 	boolean[] unusedAttr;
 	Data(double[] data,ComparatorChooser chooser,boolean[] unusedAttr){
@@ -298,6 +379,9 @@ class Data implements Comparable<Data>{
 	}
 }
 
+/**
+ * 用于选择数据比较的维度，是数据类的内部类
+ */
 class ComparatorChooser{
 	int attributeIndex;
 
--- src/search/AStar.java
+++ src/search/AStar.java
@@ -28,22 +28,29 @@ public class AStar {
         return dis;
     }
 
+    /**
+     * 拓展函数，用于拓展选中的状态
+     * @param state 当前的状态值
+     * @return 根据当前状态可以产生的状态集合
+     * @throws Exception
+     */
     private HashSet<Array> expand(Array state) throws Exception {
 
         HashSet<Array> newState=new HashSet<>();
+        //遍历寻找0所在的位置
         for(int i=0;i<size;i++) {
             for (int j=0; j < size; j++) {
-                if(state.array[i][j]==0){
-                    if(i!=0){
+                if(state.array[i][j]==0){//找到0的位置后，将其上下左右移动
+                    if(i!=0){//不在最上层
                         newState.add(stateExchange(state,i,j,0));
                     }
-                    if(i!=size-1){
+                    if(i!=size-1){//不在最下层
                         newState.add(stateExchange(state,i,j,1));
                     }
-                    if(j!=0){
+                    if(j!=0){//不在最左侧
                         newState.add(stateExchange(state,i,j,2));
                     }
-                    if(j!=size-1){
+                    if(j!=size-1){//不在最右侧
                         newState.add(stateExchange(state,i,j,3));
                     }
                     return newState;
@@ -53,51 +60,73 @@ public class AStar {
         }
         return null;
     }
-//direct指明交换的方向，0、1、2、3分别表示向上下左右交换
+    //direct指明交换的方向，0、1、2、3分别表示向上下左右交换
+
+    /**
+     * 状态改变函数，基于一定规则得出当前状态的一个下一状态
+     * @param state 当前状态
+     * @param i 当前状态中0的i值
+     * @param j 当前状态中0的j值
+     * @param direct 指明交换的方向，0、1、2、3分别表示向上下左右交换
+     * @return 按对应规则得出的下一个状态
+     * @throws Exception
+     */
     private Array stateExchange(Array state,int i,int j,int direct) throws Exception {
-        Array copy=cloneState(state);
+        Array copy=cloneState(state);//深拷贝一个原状态值出来，防止修改到原先的状态
         int buf;
         switch (direct){
-
-            case(0):
+            case(0)://将0向上移动
                 buf=state.array[i-1][j];
                 copy.array[i-1][j]=0;
                 copy.array[i][j]=buf;
                 break;
-            case(1):
+            case(1)://将0向下移动
                 buf=state.array[i+1][j];
                 copy.array[i+1][j]=0;
                 copy.array[i][j]=buf;
                 break;
-            case (2):
+            case (2)://将0向左移动
                 buf=state.array[i][j-1];
                 copy.array[i][j-1]=0;
                 copy.array[i][j]=buf;
                 break;
-            case(3):
+            case(3)://将0向右移动
                 buf=state.array[i][j+1];
                 copy.array[i][j+1]=0;
                 copy.array[i][j]=buf;
                 break;
-            default:
+            default://都不是就报错
                 throw new Exception("未知的direct！");
         }
         return copy;
     }
 
+    /**
+     * 用于深拷贝一个状态值出来，方便在原状态基础上进行修改，同时不影响真正原状态的值
+     * @param src 原状态
+     * @return 深拷贝的一个，与原状态值相同的状态
+     */
     private Array cloneState(Array src){
         Array dst=new Array(new int[src.array.length][src.array[0].length]);
         for(int i=0;i<size;i++){
+            //对二重数组内的每一个数组进行深拷贝
             System.arraycopy(src.array[i],0,dst.array[i],0,src.array[i].length);
         }
         return dst;
     }
 
+    /**
+     * 选择拓展的状态
+     * @param OPEN 选取拓展的open集
+     * @param CLOSE 已经被选过的close集
+     * @return 一个在f函数下表现最佳的状态
+     */
     public Array chooseToExpand(Map<Array,Meta> OPEN,Map<Array,Meta> CLOSE){
         int min=Integer.MAX_VALUE;
         Meta chooseMeta=null;
         Array choose=null;
         Iterator<Array> it=OPEN.keySet().iterator();
+        //根据OPEN集的迭代器，遍历OPEN集，选择f，即g+h最小的一个状态
         while (it.hasNext()){
             Array elem=it.next();
             Meta meta=OPEN.get(elem);
@@ -107,45 +136,64 @@ public class AStar {
                 choose=elem;
             }
         }
+        //将对应状态移出OPEN集并加入CLOSE集
         OPEN.remove(choose);
         CLOSE.put(choose,chooseMeta);
         return choose;
     }
+
+    /**
+     * 打印对应的一个状态
+     * @param state 对应状态值
+     */
     void printState(Array state){
         for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
+            for(int j=0;j<size;j++){//遍历进行打印
                 if(state.array[i][j]!=0){
                     System.out.print(state.array[i][j]+" ");
-                }else {
+                }else {//将0用空格代替
                     System.out.print("  ");
                 }
             }
             System.out.println();
         }
     }
+
+    /**
+     * 搜索算法
+     * @throws Exception
+     */
     public void search() throws Exception {
+        //用于存储集合中每个状态以及到达该状态的路径，Meta用于保存到达该状态的路径的父节点
         Map<Array,Meta> OPEN=new HashMap<>();
         Map<Array,Meta> CLOSE=new HashMap<>();
+        //将初始集合放入OPEN集
         OPEN.put(puzzleStart,new Meta(0,distanceH(puzzleStart),null));
+        //如果OPEN集合不为空，说明还能拓展
         while(!OPEN.isEmpty()){
+            //从OPEN集中选一个进行拓展
             Array choose=chooseToExpand(OPEN,CLOSE);
-            //printState(choose);
-            //System.out.println(CLOSE.get(choose).distanceG+"--open size "+OPEN.size());
+          //得到拓展后的状态集合
             Set<Array> expandedStates=expand(choose);
+            //g为目前为止到达该状态经历的拓展次数
             int distanceG=CLOSE.get(choose).distanceG+1;
+            //如果拓展出结束状态，将路径保存，直接退出循环
             if(expandedStates.contains(puzzleEnd)){
                 Meta meta=new Meta(distanceG,0,choose);
                 CLOSE.put(puzzleEnd,meta);
                 break;
             }
+            //如果没有拓展出结束状态，则遍历拓展出的每个状态，查看是否在OPEN集和CLOSE集中存在
             for(Array expState:expandedStates){
                 if(OPEN.containsKey(expState)){
+                    //如果在OPEN集中存在，且g大于本次的状态，那么使用此状态进行替换
                     Meta meta=OPEN.get(expState);
                     if(meta.distanceG>distanceG){
                         meta.distanceG=distanceG;
                         meta.fatherNode=choose;
                     }
                 }
+                //如果在CLOSE集中存在，且g也大于本次的状态，那么将状态重新放回OPEN集
                 if(CLOSE.containsKey(expState)) {
                     Meta meta=CLOSE.get(expState);
                     if(meta.distanceG>distanceG){
@@ -155,15 +203,18 @@ public class AStar {
                         CLOSE.remove(expState);
                     }
                 }
+                //如果都没有，那么直接放入OPEN集合
                 if((!OPEN.containsKey(expState))&&(!CLOSE.containsKey(expState))){
                     Meta meta=new Meta(distanceG,distanceH(expState),choose);
                     OPEN.put(expState,meta);
                 }
             }
         }
+        //如果OPEN集空了也没有找到，表明没有对应的路径通向终点状态
         if(OPEN.isEmpty()){
             System.out.println("没有找到合适的步骤实现转变！");
         }else{
+            //如果知道了，将路径按终点到起点的顺序打印出来
             System.out.println("从终点状态追溯到起始状态：");
             Array state=puzzleEnd;
             int step=1;
@@ -179,13 +230,13 @@ public class AStar {
 
 
     public static void main(String[] args) throws Exception {
-        int[][] start=new int[][]{{1,3,0},{8,2,4},{7,6,5}};
-        int[][] end=new int[][]{{1,2,3},{8,0,4},{7,6,5}};
-        AStar aStar=new AStar(new Array(start),new Array(end),3);
+        int[][] start=new int[][]{{5,1,2,3},{6,10,4,0},{13,9,7,8},{14,15,11,12}};
+        int[][] end=new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+        AStar aStar=new AStar(new Array(start),new Array(end),4);
         aStar.search();
     }
 }
-
+//用于封装状态的一个类，包含了此状态与对应上一个状态的联系，即父节点
 class Meta{
     int distanceG;
     int distanceH;
@@ -197,7 +248,7 @@ class Meta{
         fatherNode=father;
     }
 }
-
+//状态类
 class Array{
     int[][] array;
     Array(int[][]array){
